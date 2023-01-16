69f4746cb7178990164b563d16a4192a4a25b084
raghavendra1207 <112899843+raghavendra1207@users.noreply.github.com>
2023-01-16 11:03:40 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,16 +2,12 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+ 
     static associate(models) {
       User.hasMany(models.Todo, {
         foreignKey: "userId",
       });
-      // define association here
+     
     }
   }
   User.init(
